e59f2e4bf18f4f53b943873497eeaf4bc20da7bf
Asif Mahmud <asifmahmud802@gmail.com>
2024-08-09 21:33:11 +0600

    Done

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,50 @@
+import styles from './ProjectsStyles.module.css';
+import hotel from '../../assets/hotel.png';
+import stylora from '../../assets/shopping.png';
+import stylora2 from '../../assets/ecommerce.png';
+import fitness from '../../assets/gym.png';
+import flipkart from '../../assets/flipkart.png';
+import ProjectCard from '../../common/ProjectCard';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={hotel}
+          link="https://oficialasif.github.io/HotelBD"
+          h3="HotelBD"
+          p="A Hotel Booking Site Design"
+        />
+        <ProjectCard
+          src={stylora}
+          link="https://oficialasif.github.io/Stylora-Ecommerce"
+          h3="STYLORA-1"
+          p="An Online Shopping Site"
+        />
+        <ProjectCard
+          src={stylora2}
+          link="#"
+          h3="STYLORA-2"
+          p="An Online Shopping Site"
+        />
+        <ProjectCard
+          src={fitness}
+          link="#"
+          h3="GYMER"
+          p="A Gymnestick Website"
+        />
+        <ProjectCard
+          src={flipkart}
+          link="https://oficialasif.github.io/FlipKart-Clone"
+          h3="Flipkart CLone - Frontpage"
+          p="A non responsive front page. Only for Big screen"
+        />
+        
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
